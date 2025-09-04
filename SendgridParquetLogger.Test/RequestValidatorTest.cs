0a94b7a74e85e7ced6bec3eca5192894f1115c2c
TANABE Hiroyasu <h@cosat.com>
2025-09-04 19:41:01 +0900

    unittest

--- SendgridParquetLogger.Test/RequestValidatorTest.cs
+++ SendgridParquetLogger.Test/RequestValidatorTest.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Options;
 
 using SendgridParquet.Shared;
+
 using SendgridParquetLogger.Helper;
 
 namespace SendgridParquetLogger.Test;
@@ -114,5 +115,36 @@ public class RequestValidatorTest
         var result = validator.VerifySignature(payload, headers);
 
         Assert.That(result, Is.EqualTo(RequestValidator.RequestValidatorResult.NotConfigured));
+    }
+
+    record RequestParameters(string payload, string pem, string signature, string timestamp);
+
+    private static RequestParameters GetValidPayload() => new RequestParameters(
+        @"[{""email"":""example@test.com"",""timestamp"":1756978099,""smtp-id"":""\u003c14c5d75ce93.dfd.64b469@ismtpd-555\u003e"",""event"":""processed"",""category"":[""cat facts""],""sg_event_id"":""A46jsG3T3NmurWTDFXPkiQ=="",""sg_message_id"":""14c5d75ce93.dfd.64b469.filter0001.16648.5515E0B88.0""},{""email"":""example@test.com"",""timestamp"":1756978099,""smtp-id"":""\u003c14c5d75ce93.dfd.64b469@ismtpd-555\u003e"",""event"":""deferred"",""category"":[""cat facts""],""sg_event_id"":""j5PmIu7JliU9hYvYfc8K0Q=="",""sg_message_id"":""14c5d75ce93.dfd.64b469.filter0001.16648.5515E0B88.0"",""response"":""400 try again later"",""attempt"":""5""},{""email"":""example@test.com"",""timestamp"":1756978099,""smtp-id"":""\u003c14c5d75ce93.dfd.64b469@ismtpd-555\u003e"",""event"":""delivered"",""category"":[""cat facts""],""sg_event_id"":""8xa5uuN2-kfahpy31gL9yA=="",""sg_message_id"":""14c5d75ce93.dfd.64b469.filter0001.16648.5515E0B88.0"",""response"":""250 OK""},{""email"":""example@test.com"",""timestamp"":1756978099,""smtp-id"":""\u003c14c5d75ce93.dfd.64b469@ismtpd-555\u003e"",""event"":""open"",""category"":[""cat facts""],""sg_event_id"":""vSVz42Di1PoAteCOCWAaYQ=="",""sg_message_id"":""14c5d75ce93.dfd.64b469.filter0001.16648.5515E0B88.0"",""useragent"":""Mozilla/4.0 (compatible; MSIE 6.1; Windows XP; .NET CLR 1.1.4322; .NET CLR 2.0.50727)"",""ip"":""255.255.255.255""},{""email"":""example@test.com"",""timestamp"":1756978099,""smtp-id"":""\u003c14c5d75ce93.dfd.64b469@ismtpd-555\u003e"",""event"":""click"",""category"":[""cat facts""],""sg_event_id"":""iEYzPYOFZH84LM-7JXOLwA=="",""sg_message_id"":""14c5d75ce93.dfd.64b469.filter0001.16648.5515E0B88.0"",""useragent"":""Mozilla/4.0 (compatible; MSIE 6.1; Windows XP; .NET CLR 1.1.4322; .NET CLR 2.0.50727)"",""ip"":""255.255.255.255"",""url"":""http://www.sendgrid.com/""},{""email"":""example@test.com"",""timestamp"":1756978099,""smtp-id"":""\u003c14c5d75ce93.dfd.64b469@ismtpd-555\u003e"",""event"":""bounce"",""category"":[""cat facts""],""sg_event_id"":""6gOgJy0YoeV3l9woEHcJnA=="",""sg_message_id"":""14c5d75ce93.dfd.64b469.filter0001.16648.5515E0B88.0"",""reason"":""500 unknown recipient"",""status"":""5.0.0""},{""email"":""example@test.com"",""timestamp"":1756978099,""smtp-id"":""\u003c14c5d75ce93.dfd.64b469@ismtpd-555\u003e"",""event"":""dropped"",""category"":[""cat facts""],""sg_event_id"":""F3JgfHmSk8nwaSkFWWXqJQ=="",""sg_message_id"":""14c5d75ce93.dfd.64b469.filter0001.16648.5515E0B88.0"",""reason"":""Bounced Address"",""status"":""5.0.0""},{""email"":""example@test.com"",""timestamp"":1756978099,""smtp-id"":""\u003c14c5d75ce93.dfd.64b469@ismtpd-555\u003e"",""event"":""spamreport"",""category"":[""cat facts""],""sg_event_id"":""-qql8GWzr-QovKSuw9kQbQ=="",""sg_message_id"":""14c5d75ce93.dfd.64b469.filter0001.16648.5515E0B88.0""},{""email"":""example@test.com"",""timestamp"":1756978099,""smtp-id"":""\u003c14c5d75ce93.dfd.64b469@ismtpd-555\u003e"",""event"":""unsubscribe"",""category"":[""cat facts""],""sg_event_id"":""dMq02kRaE-Y6g0B2ouzNnw=="",""sg_message_id"":""14c5d75ce93.dfd.64b469.filter0001.16648.5515E0B88.0""},{""email"":""example@test.com"",""timestamp"":1756978099,""smtp-id"":""\u003c14c5d75ce93.dfd.64b469@ismtpd-555\u003e"",""event"":""group_unsubscribe"",""category"":[""cat facts""],""sg_event_id"":""9__KadD01Un5fxEJeTfqZA=="",""sg_message_id"":""14c5d75ce93.dfd.64b469.filter0001.16648.5515E0B88.0"",""useragent"":""Mozilla/4.0 (compatible; MSIE 6.1; Windows XP; .NET CLR 1.1.4322; .NET CLR 2.0.50727)"",""ip"":""255.255.255.255"",""url"":""http://www.sendgrid.com/"",""asm_group_id"":10},{""email"":""example@test.com"",""timestamp"":1756978099,""smtp-id"":""\u003c14c5d75ce93.dfd.64b469@ismtpd-555\u003e"",""event"":""group_resubscribe"",""category"":[""cat facts""],""sg_event_id"":""78rdgPoCKxrRzW_JKRRFug=="",""sg_message_id"":""14c5d75ce93.dfd.64b469.filter0001.16648.5515E0B88.0"",""useragent"":""Mozilla/4.0 (compatible; MSIE 6.1; Windows XP; .NET CLR 1.1.4322; .NET CLR 2.0.50727)"",""ip"":""255.255.255.255"",""url"":""http://www.sendgrid.com/"",""asm_group_id"":10}]",
+        "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAElfwhrGF18oVj/8KIQtvzNi6HxqupuFVp5iKJUiRsdEGnZKJZd31NUS/kWVXXCPO8YMlp7iX45KDz2O/oWGBVLQ==",
+        "MEUCIQDiWx4W1dVQ8M/bpIU9M6e50jFhwVzPy8EfJcG96/ea6gIgJK0+LBYqdYJJGzgXo92O1PTH161yCByYuS5VtjRGz20=",
+        "1756978099"
+    );
+
+    [Test]
+    public void StarkBankValidator_Succeeds()
+    {
+        (string payload, string pem, string signature, string timestamp) = GetValidPayload();
+        var result = StarkBankValidator.VerifySignature(payload, pem, signature, timestamp);
+
+        Assert.That(result, Is.EqualTo(true));
+    }
+
+    [Test]
+    public void Validator_Succeeds()
+    {
+        (string payloadJson, string pem, string sig, string ts) = GetValidPayload();
+        byte[] payloadBytes = Encoding.UTF8.GetBytes(payloadJson);
+        var validator = CreateValidator(pem);
+        var headers = MakeHeaders(ts, sig);
+
+        var result = validator.VerifySignature(payloadBytes, headers);
+
+        Assert.That(result, Is.EqualTo(RequestValidator.RequestValidatorResult.Verified));
     }
 }
---
+++ SendgridParquetLogger.Test/StarkBankValidator.cs
@@ -0,0 +1,14 @@
+﻿using EllipticCurve;
+
+namespace SendgridParquetLogger.Helper;
+
+public static class StarkBankValidator
+{
+    public static bool VerifySignature(string payload, string pem, string signature, string timestamp)
+    {
+        PublicKey? publicKey = PublicKey.fromPem(pem);
+        string timestampedPayload = timestamp + payload;
+        Signature? decodedSignature = Signature.fromBase64(signature);
+        return Ecdsa.verify(timestampedPayload, decodedSignature, publicKey);
+    }
+}
