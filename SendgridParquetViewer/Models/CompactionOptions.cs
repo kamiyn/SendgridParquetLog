e7e4ceb67ee5644749620e074fef92b46c6e0b1d
TANABE Hiroyasu <h@cosat.com>
2025-09-21 23:57:01 +0900

    Compaction バイト数を減らす

--- SendgridParquetViewer/Models/CompactionOptions.cs
+++ SendgridParquetViewer/Models/CompactionOptions.cs
@@ -5,7 +5,10 @@ public class CompactionOptions
     public const string SectionName = "Compaction";
 
     /// <summary>
-    /// 最大読み込みバイト数 (デフォルト: 512MB)
+    /// 最大読み込みバイト数 (デフォルト: 256MB)
+    /// 
+    /// アプリケーション自体が 300MB 程度消費し
+    /// 読み込みと 書き込みのためのバッファで概ね2倍使うため 1GB インスタンスの場合最大 350MB 程度となる
     /// </summary>
-    public long MaxBatchSizeBytes { get; set; } = 512 * 1024 * 1024;
+    public long MaxBatchSizeBytes { get; set; } = 256 * 1024 * 1024;
 }
