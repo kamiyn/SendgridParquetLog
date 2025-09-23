d7514ac8f288ecb72db916ed4db0e250beea8b8e
TANABE Hiroyasu <h@cosat.com>
2025-09-24 03:10:51 +0900

    sanitize

--- SendgridParquetViewer/Services/DisposableTempFile.cs
+++ SendgridParquetViewer/Services/DisposableTempFile.cs
@@ -5,10 +5,18 @@ public static class DisposableTempFile
     public const int BufferSize = 65536;
 
     public static FileStream Open(string filename) =>
-        new(Path.Combine(Path.GetTempPath(), $"{filename}-{Path.GetRandomFileName()}"),
+        new(GetPath(filename),
             FileMode.Create,
             FileAccess.ReadWrite,
             FileShare.None,
             BufferSize,
             options: FileOptions.DeleteOnClose);
+
+    private static readonly char[] s_invalidChars = Path.GetInvalidFileNameChars();
+
+    private static string GetPath(string filename)
+    {
+        string sanitizedFileName = new string( filename.Select(c => s_invalidChars.Contains(c) ? '_' : c).ToArray());
+        return Path.Combine(Path.GetTempPath(), $"{sanitizedFileName}-{Path.GetRandomFileName()}");
+    }
 }
