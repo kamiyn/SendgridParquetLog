ad8e20802421e0df4205a9a13de04e9d7818907d
TANABE Hiroyasu <h@cosat.com>
2025-09-25 10:15:10 +0900

    Periodic Run Compaction

--- SendgridParquetViewer/Services/CompactionStartupHostedService.cs
+++ SendgridParquetViewer/Services/CompactionStartupHostedService.cs
@@ -1,16 +1,48 @@
-﻿namespace SendgridParquetViewer.Services;
+﻿using System.Diagnostics;
+
+using SendgridParquetViewer.Models;
+
+namespace SendgridParquetViewer.Services;
 
 public sealed class CompactionStartupHostedService(
     CompactionService compactionService
 ) : IHostedService, IAsyncDisposable
 {
-    public async Task StartAsync(CancellationToken ct)
+    private readonly TimeSpan _periodicSpan = TimeSpan.FromDays(1);
+    private CancellationTokenSource? _cts;
+    private CompactionStartResult? _compactionStartResult;
+
+    public Task StartAsync(CancellationToken ct)
     {
-        await compactionService.StartCompactionAsync(ct);
+        _cts = CancellationTokenSource.CreateLinkedTokenSource(ct);
+        _ = Task.Run(async () =>
+        {
+            Stopwatch stopwatch = new();
+            do
+            {
+                stopwatch.Restart();
+                _compactionStartResult = await compactionService.StartCompactionAsync(ct);
+                if (_compactionStartResult?.StartTask != null)
+                {
+                    await _compactionStartResult.StartTask;
+                }
+
+                TimeSpan elapsed = stopwatch.Elapsed;
+                if (elapsed < _periodicSpan)
+                {
+                    await Task.Delay(elapsed - _periodicSpan, _cts.Token);
+                }
+            } while (!_cts.IsCancellationRequested);
+        }, _cts.Token);
+        return Task.CompletedTask;
     }
 
     public async Task StopAsync(CancellationToken ct)
     {
+        if (_cts != null)
+        {
+            await _cts.CancelAsync();
+        }
         await compactionService.StopCompactionAsync(ct);
     }
 
