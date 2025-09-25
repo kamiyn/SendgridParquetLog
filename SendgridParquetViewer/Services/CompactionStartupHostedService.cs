f4d7609d82a9cb3d1c1179bd500ac8f75021c306
TANABE Hiroyasu <h@cosat.com>
2025-09-25 12:39:44 +0900

    UseBackgroundService

--- SendgridParquetViewer/Services/CompactionStartupHostedService.cs
+++ SendgridParquetViewer/Services/CompactionStartupHostedService.cs
@@ -5,11 +5,21 @@ namespace SendgridParquetViewer.Services;
 public sealed class CompactionStartupHostedService(
     ILogger<CompactionStartupHostedService> logger,
     CompactionService compactionService
-) : IHostedService, IAsyncDisposable
+) : BackgroundService
 {
     private readonly TimeSpan _periodicSpan = TimeSpan.FromDays(1);
-    private CancellationTokenSource? _cts;
-    private Task? _loopTask;
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        await Run(stoppingToken);
+        using var timer = new PeriodicTimer(_periodicSpan);
+        while (await timer.WaitForNextTickAsync(stoppingToken))
+        {
+            await Run(stoppingToken);
+        }
+
+        await compactionService.StopCompactionAsync(CancellationToken.None);
+    }
 
     private async Task Run(CancellationToken ct)
     {
@@ -27,59 +37,5 @@ public sealed class CompactionStartupHostedService(
         {
             logger.ZLogError(ex, $"CompactionStartupHostedService");
         }
-    }
-
-    public Task StartAsync(CancellationToken ct)
-    {
-        _cts = CancellationTokenSource.CreateLinkedTokenSource(ct);
-        _ = Run(_cts.Token).ContinueWith(t =>
-        {
-            if (t.Exception != null)
-            {
-                logger.ZLogError(t.Exception, "Exception in initial compaction Run task");
-            }
-        }, TaskContinuationOptions.OnlyOnFaulted);
-        _loopTask = Task.Run(async () =>
-        {
-            using var timer = new PeriodicTimer(_periodicSpan);
-            while (await timer.WaitForNextTickAsync(_cts.Token))
-            {
-                await Run(_cts.Token);
-            }
-        }, _cts.Token);
-        return Task.CompletedTask;
-    }
-
-    public async Task StopAsync(CancellationToken ct)
-    {
-        if (_cts != null)
-        {
-            await _cts.CancelAsync();
-        }
-        await compactionService.StopCompactionAsync(ct);
-        if (_loopTask != null)
-        {
-            try
-            {
-                await _loopTask;
-            }
-            catch (OperationCanceledException) { }
-            catch (Exception ex)
-            {
-                logger.ZLogError(ex, $"Error occurred during hosted service shutdown in StopAsync");
-            }
-        }
-    }
-
-    public async ValueTask DisposeAsync()
-    {
-        if (_cts != null)
-        {
-            await _cts.CancelAsync();
-            _cts.Dispose();
-            _cts = null;
-        }
-        // Dispose is called after StopAsync, so no need to call StopCompactionAsync here.
-        // await compactionService.StopCompactionAsync(CancellationToken.None);
     }
 }
