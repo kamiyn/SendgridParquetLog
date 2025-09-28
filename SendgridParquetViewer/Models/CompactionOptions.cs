39a99d9a1ae86f933474a9cdef3f1d6a4b3586ee
TANABE Hiroyasu <h@cosat.com>
2025-09-28 17:50:48 +0900

    delete unused option

--- SendgridParquetViewer/Models/CompactionOptions.cs
+++ SendgridParquetViewer/Models/CompactionOptions.cs
@@ -10,11 +10,6 @@ public class CompactionOptions
     public long MaxBatchSizeBytes { get; set; } = 512 * 1024 * 1024;
 
     /// <summary>
-    /// メモリ上で保持する最大イベント数
-    /// </summary>
-    public int MaxEventsPerChunk { get; set; } = 10_000;
-
-    /// <summary>
     /// 標準では JST基準で昨日以前のものが対象になる
     /// </summary>
     public TimeSpan TargetBefore { get; set; } = TimeSpan.FromDays(-1);
