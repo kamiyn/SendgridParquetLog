6346d710e7ff24caac5a72a4695c094d36d0a9fc
TANABE Hiroyasu <h@cosat.com>
2026-02-13 11:09:29 +0900

    SendgridParquetViewer/Components/Pages/Compaction.razor
    時折 Compaction 処理が停止してしまうことがあります。Dockerインスタンスが終了することが原因として考えられます。
    S3 上にあるロックファイル run.lock を削除する 機能を実装をしてください
    
    - 自プロセス(インスタンス)でスレッド進行中であることが観測できれば この機能のためのボタンは無効としてください。
    - RunStatus の Start Time が24時間以内であれば無効としてください。

--- SendgridParquetViewer/Services/S3LockService.cs
+++ SendgridParquetViewer/Services/S3LockService.cs
@@ -13,6 +13,7 @@ public interface IS3LockService
     Task<bool> TryAcquireLockAsync(string lockId, CancellationToken ct);
     Task ExtendLockExpirationAsync(string lockId, CancellationToken ct);
     Task ReleaseLockAsync(string lockId, CancellationToken ct);
+    Task ForceDeleteLockAsync(CancellationToken ct);
 }
 
 public class S3LockService(
@@ -134,5 +135,12 @@ public class S3LockService(
                 logger.ZLogInformation($"Lock released successfully. Lock ID: {lockId} lockPath:{lockPath}");
             }
         }
+    }
+
+    public async Task ForceDeleteLockAsync(CancellationToken ct)
+    {
+        var (_, lockPath) = SendGridPathUtility.GetS3CompactionRunFile();
+        await s3StorageService.DeleteObjectAsync(lockPath, ct);
+        logger.ZLogWarning($"Lock file force-deleted. lockPath:{lockPath}");
     }
 }
